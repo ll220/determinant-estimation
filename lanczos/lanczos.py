17d8cbf5629ea030b72edf9c31a25490452c3cd4
Lucy <lucylin220@gmail.com>
2023-08-01 21:12:57 -0800

    bug fixing and sample graphs

--- lanczos/lanczos.py
+++ lanczos/lanczos.py
@@ -11,21 +11,17 @@ from scipy.linalg import eigh_tridiagonal
 
 LANCZOS_LOGGING = False
 VALUES_LOGGING = False
-E_LOGGING = True
+E_LOGGING = False
 
-NUM_V = 1
-DIM = 5
-N = 5
+# NUM_V = 30
+# DIM = 5 # dimensions of input matrix
+# M = 5 # num of lanczos iterations
 
-RANDOM_MEAN = 5
+RANDOM_MEAN = 0
 RANDOM_ST = 5
 
-E_ONE_T = np.array([[1] + [0] * (N - 1)])
-
-DET_EST = 0
-
-def set_up_a_matrix():
-    triangle_matrix = generate_lower_triangle_matrix()
+def set_up_a_matrix(dim):
+    triangle_matrix = generate_lower_triangle_matrix(dim)
     transpose = triangle_matrix.transpose()
     
     pos_semi_def_matrix = np.matmul(triangle_matrix, transpose)
@@ -35,24 +31,24 @@ def set_up_a_matrix():
     # print("\n\n Random Positive Semi-Definite matrix size ", size, ": \n", pos_semi_def_matrix)
     return pos_semi_def_matrix
 
-def generate_lower_triangle_matrix():
-    triangle_matrix = np.zeros(shape=(DIM, DIM))
-    for x in range(DIM):
-        for y in range(DIM - x):
-            triangle_matrix[DIM - x - 1][y] = abs(np.random.normal(loc=RANDOM_MEAN, scale=RANDOM_ST, size=None))
+def generate_lower_triangle_matrix(dim):
+    triangle_matrix = np.zeros(shape=(dim, dim))
+    for x in range(dim):
+        for y in range(dim - x):
+            triangle_matrix[dim - x - 1][y] = np.random.normal(loc=RANDOM_MEAN, scale=RANDOM_ST, size=None)
 
-            # if (DIM - x - 1) == y and triangle_matrix[DIM - x - 1][y] < 0.0:
-            #     triangle_matrix[DIM - x - 1][y] = 1.0
+            if (dim - x - 1) == y and triangle_matrix[dim - x - 1][y] < 0.0:
+                triangle_matrix[dim - x - 1][y] = 1.0
 
     return triangle_matrix
 
 
-def generate_rademacher_vector_and_q1():
-    list = np.array([np.random.choice([1, -1], p=[0.5, 0.5], size=(DIM))])
+def generate_rademacher_vector_and_q1(dim):
+    list = np.array([np.random.choice([1, -1], p=[0.5, 0.5], size=(dim))])
     b_vector = list.transpose()
 
     b_magnitude = 0
-    for i in range(DIM):
+    for i in range(dim):
         b_magnitude += pow(b_vector[i][0], 2)
 
     b_magnitude = math.sqrt(b_magnitude)
@@ -61,18 +57,18 @@ def generate_rademacher_vector_and_q1():
     return b_vector, q1
 
 
-def lanczos_iteration(a, q1):
+def lanczos_iteration(dim, m, a, q1):
     tridiag_matrix = []
     q_matrix = q1
 
-    q_n_minus_1 = np.array([[0] * DIM])
+    q_n_minus_1 = np.array([[0] * dim])
     q_n_minus_1 = q_n_minus_1.transpose()
 
     qn = q1
     beta_n_minus_1 = 0
     beta_n = 0
 
-    for n in range(1, N + 1):
+    for n in range(1, m + 1):
         if(LANCZOS_LOGGING):
             print("iteration n = ", str(n))
 
@@ -89,9 +85,9 @@ def lanczos_iteration(a, q1):
             print(qn)
             print("\n")
 
-        tridiag_matrix = append_tridiag_matrix(tridiag_matrix, n, alpha_n, beta_n, beta_n_minus_1)
+        tridiag_matrix = append_tridiag_matrix(dim, tridiag_matrix, n, alpha_n, beta_n, beta_n_minus_1)
 
-        if (n < N):
+        if (n < m):
             q_n_plus_1 = np.divide(v, beta_n)
             q_n_minus_1 = qn
             qn = q_n_plus_1
@@ -103,17 +99,17 @@ def lanczos_iteration(a, q1):
     return q_matrix, np.array(tridiag_matrix)
 
 
-def append_tridiag_matrix(tridiag_matrix, n, alpha_n, beta_n, beta_n_minus_1):
+def append_tridiag_matrix(dim, tridiag_matrix, n, alpha_n, beta_n, beta_n_minus_1):
     row = []
 
     if (n == 1):
         row.append(alpha_n)
         row.append(beta_n)
-        last_half = [0] * (DIM - 2)
+        last_half = [0] * (dim - 2)
         row = row + last_half
 
-    elif (n == DIM):
-        first_half = [0] * (DIM - 2)
+    elif (n == dim):
+        first_half = [0] * (dim - 2)
         row = row + first_half
         row.append(beta_n_minus_1)
         row.append(alpha_n)
@@ -124,71 +120,107 @@ def append_tridiag_matrix(tridiag_matrix, n, alpha_n, beta_n, beta_n_minus_1):
         row.append(beta_n_minus_1)
         row.append(alpha_n)
         row.append(beta_n)
-        second_half = [0] * (DIM - n - 1)
+        second_half = [0] * (dim - n - 1)
         row = row + second_half
 
     tridiag_matrix.append(row)
     return tridiag_matrix
 
+def estimate_determinant(num_v, dim, m):
+
+    a = set_up_a_matrix(dim)
+
+    # act_start_time = time.time()
+    (sign, logabsdet) = np.linalg.slogdet(a)
+    act_det = sign * logabsdet
+    # act_end_time = time.time()
+
+    # act_time = act_end_time - act_start_time
+
+    # est_start_time = time.time()
+    det_est = 0
+    for i in range(num_v):
+        input_vector, q1 = generate_rademacher_vector_and_q1(dim)
+
+        q_matrix, tridiag_matrix = lanczos_iteration(dim, m, a, q1)
+
+        d = []
+        e = []
+
+        for n in range(m):
+            d.append(tridiag_matrix[n][n])
+
+        for n in range(m - 1):
+            e.append(tridiag_matrix[n][n+1])
+
+        evalues, evectors = eigh_tridiagonal(d, e)
+
+        if(VALUES_LOGGING):
+            print("a: ")
+            print(a)
+            print("q matrix: ")
+            print(q_matrix)
+            print("\n")
 
 
-for i in range(NUM_V):
-    input_vector, q1 = generate_rademacher_vector_and_q1()
-    q_matrix = q1
-    a = set_up_a_matrix()
+            print("tridiagonal matrix: ")
+            print(tridiag_matrix)
+            print("\n")
 
-    q_matrix, tridiag_matrix = lanczos_iteration(a, q1)
+            # print("sub tridiagonal matrix: ")
+            # print(sub_tridiag_matrix)
+            # print("\n")
 
-    sub_tridiag_matrix = tridiag_matrix[0:N, 0:N]
+            test_matrix = np.matmul(q_matrix.transpose(), a)
+            test_matrix = np.matmul(test_matrix, q_matrix)
 
-    d = []
-    e = []
+            print("test matrix: ")
+            print(test_matrix)
+            print("\n")
 
-    for n in range(N):
-        d.append(sub_tridiag_matrix[n][n])
+        if (E_LOGGING):
 
-    for n in range(N - 1):
-        e.append(sub_tridiag_matrix[n][n+1])
+            print("Eigenvalues of a: ")
+            print(np.linalg.eigvalsh(a))
 
-    evalues, evectors = eigh_tridiagonal(d, e)
-    
-    if(VALUES_LOGGING):
-        print("a: ")
-        print(a)
-        print("q matrix: ")
-        print(q_matrix)
-        print("\n")
+            print("\nEigenvalues of tridiag: ")
+            print(evalues)
+            print("\nEigenvectors of tridiag: ")
+            print(evectors)
+
+        for k in range(m):
+            det_est = det_est + (evectors[k][0] * evectors[k][0] * np.log(evalues[k]))
+
+    est_det = float(dim / num_v) * det_est
+    # est_end_time = time.time()
+    # est_time = est_end_time - est_start_time
+    error = est_det - act_det
+    return error
+    # print(det_est)
+    # print(sign, logabsdet)
+
+# act_times = []
+# est_times = []
+error_vals = []
+dims = []
+
+for x in range(5, 70, 5):
+
+    error = estimate_determinant(30, 70, x)
+    error_vals.append(error)
+    # act_times.append(act_time)
+    # est_times.append(est_time)
+    dims.append(x)
 
 
-        print("tridiagonal matrix: ")
-        print(tridiag_matrix)
-        print("\n")
+plot_title = "Error vs. m iterations for Matrix size 70"
 
-        print("sub tridiagonal matrix: ")
-        print(sub_tridiag_matrix)
-        print("\n")
+plt.plot(dims, error_vals, label = "Standard Calc Times")
+# plt.plot(dims, est_times, label = "Lanczos Calc Times")
+# plt.legend()
+plt.title(plot_title)
+plt.xlabel('Iterations')
+plt.ylabel('Error')
+plt.savefig('Increasing_iterations.png')
 
-        test_matrix = np.matmul(q_matrix.transpose(), a)
-        test_matrix = np.matmul(test_matrix, q_matrix)
-
-        print("test matrix: ")
-        print(test_matrix)
-        print("\n")
-
-    if (E_LOGGING):
-
-        print("Eigenvalues of a: ")
-        print(np.linalg.eigvalsh(a))
-
-        print("\nEigenvalues of tridiag: ")
-        print(evalues)
-        print("\nEigenvectors of tridiag: ")
-        print(evectors)
-
-    for k in range(N):
-        DET_EST = DET_EST + (evectors[k][0] * evectors[k][0] * np.log(evalues[k]))
-
-DET_EST = float(DIM / NUM_V) * DET_EST
-(sign, logabsdet) = np.linalg.slogdet(a)
-print(DET_EST)
-print(logabsdet)
+plt.show()
